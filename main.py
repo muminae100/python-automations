047f72eee5d1743c7356595e7a96a8cb7cb6e12c
muminae100 <smuminaetx100@gmail.com>
2022-01-16 16:52:55 +0300

    Create extract_news function to scrape content from HN news

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #http requests
+from urllib import response
 import requests
 #web scraping
 from bs4 import BeautifulSoup
@@ -13,3 +14,21 @@ now = datetime.datetime.now()
 
 #email content placeholder
 content = ''
+
+#extracting Hacker News Stories
+def extract_news(url):
+    print('Extracting Hacker News Stories')
+    cnt = ''
+    cnt += ('<b>HN Top Stories:</b>\n' + '<br>' + '-'*50+'<br>')
+    response = requests.get(url)
+    content = response.content
+    soup  = BeautifulSoup(content, 'html.parser')
+    for i,tag in enumerate(soup.find_all('td', attrs={'class':'title','valign':''})):
+        cnt += ((str(i+1)+' :: '+ tag.text + "\n" + '<br>') if tag.text != 'More' else '')
+    return (cnt)
+
+cnt = extract_news('https://news.vcombinator.com/')
+content += cnt
+content += ('<br>-------<br>')
+content += ('<br><br>End of Message')
+
